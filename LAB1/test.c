646ba0b2d6bda842970ef760aaa0a9f11730d49c
Bartłomiej Babicz <102216334+b-babicz@users.noreply.github.com>
2023-03-29 12:53:35 +0200

    Upload test.c

---
+++ LAB1/test.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <math.h>
+main ()
+{
+	/*
+	PROGRAM 1
+	int h=24;
+	int m=60;
+	int s=60;
+	printf ("ilosc minut w 24 godzinach: %d",h*m*s);*/
+	
+	/*
+	PROGRAM 2
+	int a;
+	int b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("suma liczb wynosi: %d\n",a+b);
+	printf("roznica liczb wynosi %d\n",a-b);
+	printf("iloczyn liczb wynosi %d\n",a*b);
+	printf("iloraz liczb wynosi %f\n",a/(float)b);*/
+	
+	
+	/* PROGRAM 5
+	int x;
+	int y;
+	scanf("%d",&x);
+	scanf("%d",&y);
+	printf("srednia arytmetyczna liczb wynosi: %f\n", (float)(x+y)/2);
+	printf("srednia geometryczna wynosi: %f\n",sqrt(x*y));
+	printf("srednia harmoniczna wynosi %f\n",2/((1/(float)x)+1/(float)y));*/
+	
+	/*
+	PROGRAM 3
+	int r;
+	int H;
+	int l;
+	scanf("%d",&r);
+	scanf("%d",&H);
+	scanf("%d",&l);
+	printf("pole powierzchni sfery o zadanym promieniu wynosi %f\n", 4*(float)r*r*M_PI);
+	printf("pole kola o zadanym promieniu wynosi %f\n",M_PI*(float)r*r);
+	printf("objetosc stozka o zadanych danych wynosi %f\n",(M_PI*(float)r*r*H)/3);
+	printf("pole stozka o zadanych danych wynosi %f\n",(M_PI*(float)r*r)+(M_PI*r*l));
+	printf("objetosc kuli o zadanym promieniu wynosi %f\n",(4*M_PI*(float)r*r*r)/3);
+	printf("obwod kola o zadanym promieniu wynosi %f\n",2*M_PI*(float)r);
+	printf("pole walca o zadanych danych wynosi %f\n",2*M_PI*(float)r*(r+H));*/
+	
+	/*
+	SKLADNIA IF ELSE
+	int a=3, b=5;
+	if (a>b) {
+		printf("a jest wieksze");
+	}
+	else
+	{ printf("b jest wieksze");
+	}*/
+
+	/*
+	SKLADNIA SWITCH
+	int a=1;
+	switch(a){
+		case 1:printf("1");
+		break;
+		case 2:printf("2");
+		break;
+		case 3:printf("3");
+		break;
+		default:printf("blad");
+		break;
+	}*/
+	
+	
+	
+	
+}
